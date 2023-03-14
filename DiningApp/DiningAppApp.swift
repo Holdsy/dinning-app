5199fad840c57d0fd731ae8ea50e7995200978f4
Mark Holdsworth <43139034+Holdsy@users.noreply.github.com>
2023-03-14 18:50:35 +0000

    Initial Commit

---
+++ DiningApp/DiningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiningAppApp.swift
+//  DiningApp
+//
+//  Created by Mark Holdsworth on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
